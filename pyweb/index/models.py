76e70cbdda5f2835864553482a0c7dc3616790f2
ImranLokmic <imranlokmic99@gmail.com>
2020-03-16 23:23:25 +0100

    various experiments and tests

--- pyweb/index/models.py
+++ pyweb/index/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class card(models.Model):
+    card = models.CharField(max_length=4)
+    Card_Number = models.IntegerField()
+    PIN = models.IntegerField()
+    CVV = models.IntegerField()
+    Balance = models.IntegerField()
+    Month = models.IntegerField()
+    Year = models.IntegerField()
+    Name = models.CharField(max_length=250)
+    Address = models.CharField(max_length=500)
+    Country = models.CharField(max_length=500)
